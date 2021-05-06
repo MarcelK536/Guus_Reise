45d014d3c2b1352d279b2b7bc2571f2c7dc5e151
Max <stollrak@web.de>
2021-05-07 01:58:22 +0200

    erste version ohne exception (es wird ein bild ausgegeben)

--- Game1.cs
+++ Game1.cs
@@ -8,7 +8,7 @@ namespace Guus_Reise
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private Tile[,] _board;
+        private Tile[,] _board; //Spielbrett
         private Camera _camera;
 
         public Game1()
@@ -20,7 +20,7 @@ namespace Guus_Reise
 
         protected override void Initialize()
         {
-            int[,] tilemap = new int[,] { { 1, 1, 1, 1, 1 }, { 1, 1, 0, 0, 1 }, { 0, 0, 0, 0, 0 }, { 0, 0, 2, 2, 0 }, { 0, 0, 0, 2, 0 } };
+            int[,] tilemap = new int[,] { { 1, 1, 1, 1, 1 }, { 1, 1, 0, 0, 1 }, { 0, 0, 0, 0, 0 }, { 0, 0, 2, 2, 0 }, { 0, 0, 0, 2, 0 } }; //input Array der die Art der Tiles für die map generierung angibt
             createboard(tilemap);
 
             base.Initialize();
@@ -61,6 +61,7 @@ namespace Guus_Reise
 
         public void createboard(int[,] tilemap)
         {
+            _board = new Tile[tilemap.GetLength(0),tilemap.GetLength(1)];
             for(int i = 0; i < tilemap.GetLength(0); i++)
             {
                 for(int k =0; k < tilemap.GetLength(1); k++)
@@ -71,7 +72,7 @@ namespace Guus_Reise
                     }
                     else
                     {
-                        _board[i, k] = new Tile(new Vector3(i - tilemap.GetLength(0) / 2 - 0.5f, 0, k - tilemap.GetLength(1) / 2 - 0.5f), new Point(i, k), tilemap[i, k], Content);
+                        _board[i, k] = new Tile(new Vector3(i - tilemap.GetLength(0) / 2, 0, k - tilemap.GetLength(1) / 2-0.5f), new Point(i, k), tilemap[i, k], Content);
                     }
                 }
             }
