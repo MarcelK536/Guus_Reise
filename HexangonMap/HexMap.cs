75709aeb61133eb648e22c6cf0ae7036198dc141
niecola <kalocin@t-online.de>
2021-06-12 13:33:35 +0200

    Zuordnung passender Animationen

--- HexangonMap/HexMap.cs
+++ HexangonMap/HexMap.cs
@@ -33,7 +33,7 @@ namespace Guus_Reise
             int[,] tilemap = new int[,] { { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 } }; //input Array der die Art der Tiles für die map generierung angibt
             //int[,] charakter = new int[,] { { 20, 10, 8, 5, 5, 8, 2, 5, 0, 0 }, { 20, 7, 8, 9, 8, 8, 2, 4, 1, 1 }, { 20, 7, 8, 9, 8, 8, 2, 4, 0, 0 } };         //input Array für die Charaktere
             int[] charakterlevel = new int[] { 5, 4, 4 };
-            string[] names = new string[] { "Guu", "Timmae", "Peter" };       //input Array für Namen
+            string[] names = new string[] { "Guu", "Timmae", "Paul" };       //input Array für Namen
             int[,] charPositions = new int[,] { { 0, 1 }, { 4, 4 }, { 4, 2 } };   //input Array für Positionen
 
             Createboard(tilemap, Content);
