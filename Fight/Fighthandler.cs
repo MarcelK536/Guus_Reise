b5fb4b3cedeffc9f5bc264edec7137145b0af0ce
Marcel <marcelk1998@outlook.de>
2021-07-26 12:42:33 +0200

    Turnbar: Dead Characters are now Removed

--- Fight/Fighthandler.cs
+++ Fight/Fighthandler.cs
@@ -176,6 +176,7 @@ namespace Guus_Reise
                 {
                     if (hexTiles.Charakter.CurrentFightStats[0] <= 0)
                     {
+                        turnBar.RemoveCharakter(hexTiles.Charakter);
                         hexTiles.Charakter = null;
                     }
                 }
